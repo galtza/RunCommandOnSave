ed9a21b90d678388870b4af43bef5882eac53df8
QCSTUDIO\34629 <igaltza@gmail.com>
2022-03-13 13:49:42 +0100

    Added some links to the description of the extension and modified the guids

--- Src/Package.cs
+++ Src/Package.cs
@@ -10,7 +10,7 @@ namespace RunCommandOnSave
 {
     [PackageRegistration(UseManagedResourcesOnly = false, AllowsBackgroundLoading = true, RegisterUsing = RegistrationMethod.Assembly)]
     [ProvideAutoLoad(UIContextGuids80.NoSolution, PackageAutoLoadFlags.BackgroundLoad)]
-    [Guid("be7475e2-c591-4c89-b43e-b6b5b770aa3c")]
+    [Guid("035efc93-c6ac-4591-a200-b58286bde51f")]
     public sealed class Package : ToolkitPackage
     {
         protected override async System.Threading.Tasks.Task InitializeAsync(CancellationToken cancellationToken, IProgress<ServiceProgressData> progress)
